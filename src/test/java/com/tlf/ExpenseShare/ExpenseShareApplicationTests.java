873c934bac2859bc2ad91f1f257517aac27e0305
Vishal Dani <vishal.dani123@gmail.com>
2023-10-04 22:58:04 -0700

    Initial Commit

---
+++ src/test/java/com/tlf/ExpenseShare/ExpenseShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlf.ExpenseShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
